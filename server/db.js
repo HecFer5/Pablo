3b6c612f1eee9226d5fbdaf1ec0c21e49fd2a3c7
HecFer5 <hecalfer@hotmail.com>
2024-02-03 10:10:47 -0300

    agregando variables de entorno

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
+
+
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   port: 3306,
-  database: "pablo"
+  database: process.env.DB_DBNAME
 });
-
