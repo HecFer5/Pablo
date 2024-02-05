ddd1095b8b1e0af620b2decf015881f09b0c7534
HecFer5 <hecalfer@hotmail.com>
2024-02-05 10:54:52 -0300

    haciendo correcciones

--- server/db.js
+++ server/db.js
@@ -2,10 +2,18 @@
 
 import { createPool } from "mysql2/promise";
 
+// export const pool = createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   port: 3306,
+//   database: process.env.DB_DBNAME
+// });
+
 export const pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  host: "localhost",
+  user: "root",
+  password: "",
   port: 3306,
-  database: process.env.DB_DBNAME
+  database: "pablo"
 });
