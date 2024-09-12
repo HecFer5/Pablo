17d95e0c077e1cefc3d369f299b270b77c39e39b
hectorf <ferreccioe@gmail.com>
2024-09-12 13:49:37 -0300

    nuevo deploy

--- server/db.js
+++ server/db.js
@@ -7,21 +7,21 @@ config();
 
 
 
-// export const pool = createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   port: 3306,
-//   database: process.env.DB_DBNAME
-// });
-
-
-
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   port: 3306,
-  database: "pablo"
+  database: process.env.DB_DBNAME
 });
+
+
+
+// export const pool = createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   port: 3306,
+//   database: "pablo"
+// });
 
